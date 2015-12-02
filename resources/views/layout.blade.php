8fb335472fdb83b62fdc4a6d8dc32a549b49b079
Magikus <daniel.dourdaine@gmail.com>
2015-12-02 21:02:09 +0400

    Episode 3 : Basic Gulp Housekeeping

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Project Flyer</title>
+	<link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+
+<div class="container">
+	@yield('content')
+</div>
+
+</body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+	<h1>Project Flyer</h1>
+@stop
